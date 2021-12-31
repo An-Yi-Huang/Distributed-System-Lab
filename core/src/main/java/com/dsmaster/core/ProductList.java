45559a77577c67372777de8307c7dd3f54935413
AnYiHuang <anyi.huang@ucdconnect.ie>
2021-12-31 10:30:07 +0000

    add Clothes and Digital products

--- core/src/main/java/com/dsmaster/core/ProductList.java
+++ core/src/main/java/com/dsmaster/core/ProductList.java
@@ -27,6 +27,20 @@ public class ProductList {
             add(new ProductInfo(17, "Gordons Gin 1L", 35.00, 423, "Alcohol", 800));
             add(new ProductInfo(18, "Jameson Irish Whiskey 1L", 43.00, 673, "Alcohol", 827));
             add(new ProductInfo(19, "Baileys Original Irish Cream Liqueur 1L", 28.00, 324, "Alcohol", 676));
+            // Clothes & Digital
+            add(new ProductInfo(20, "Men's Closed Bottom Fleece Sweatpants", 19.00, 201, "Clothes", 300));
+            add(new ProductInfo(21, "Baby Girls' Playwear Set", 21.00, 352, "Clothes", 202));
+            add(new ProductInfo(22, "Women's Benton Springs Fleece Jacket", 39.00, 423, "Clothes", 30));
+            add(new ProductInfo(23, "Yoga Pants for Women", 18.00, 154, "Clothes", 80));
+            add(new ProductInfo(24, "Long Sleeve Henley Shirt", 25.00, 271, "Clothes", 50));
+            add(new ProductInfo(25, "Button Up Long Sleeve Tops", 45.00, 102, "Clothes", 90));
+            add(new ProductInfo(26, "$25 Xbox Gift Card", 25.00, 150, "Digital", 100));
+            add(new ProductInfo(27, "Women's Digital Chronograph Resin Strap Watch", 14.00, 75, "Digital", 80));
+            add(new ProductInfo(28, "Cordless Phone System", 29.50, 16, "Digital", 70));
+            add(new ProductInfo(29, "Wireless in-Ear Headphones (Black)", 158.00, 63, "Digital", 90));
+            add(new ProductInfo(30, "Wireless Bluetooth Speaker", 25.99, 24, "Digital", 36));
+
+
         }
     };
 }
